12fc653e8a1ef839d462ba937de5552caba46635
saividhyanandh <arjunsai.learn@gmail.com>
2026-02-09 07:58:53 +0530

    Revise interview questions and add SQL answers
    Updated format and added SQL answers for clarity.

---
+++ Interview Questions Basic.sql
@@ -0,0 +1,35 @@
+Here are 100+ MySQL interview questions with their answers, ranging from basic to more intermediate topics:
+
+
+## Question 1: How do you create a new database in MySQL?
+
+Answer: 
+
+CREATE DATABASE database_name;
+
+## Question 2. How do you create a new table in MySQL?
+
+Answer:
+  
+INSERT INTO table_name (column1, column2, ...)
+VALUES (value1, value2, ...);
+
+## Question 3. How do you insert values into a table?
+
+Answer:
+  
+INSERT INTO table_name (column1, column2, ...)
+VALUES (value1, value2, ...);
+
+## Question  4. How do you retrieve all the columns from a table?
+  
+Answer:
+SELECT * FROM table_name;
+
+## Question  5. How can you retrieve specific columns from a table?
+  
+Answer:
+SELECT column1, column2 
+FROM table_name;
+
+
